67d6abb2798e109090831686ff57246dd8ef773a
Salem Morelli <sm2417@vm-shell2.doc.ic.ac.uk>
2017-12-21 14:25:28 +0000

    Project_Euler

---
+++ ProjectEuler_Question002.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    int num1 = 0;
+    int num2 = 1;
+    int next = 0;
+    int sum = 0;
+
+    cout<<num1<<endl;
+    cout<<num2<<endl;
+    while(next<4000000){
+
+
+        next = num2 + num1;
+
+
+        if(next % 2 == 0){
+            sum = sum + next;
+            cout<<sum<<endl;
+        }
+        num1 = num2;
+        num2 = next;
+    }
+    return 0;
+    }
